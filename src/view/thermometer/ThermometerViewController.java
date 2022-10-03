a4fb561a49db6b00a11f4e09556013dbf609f897
dianasuzana <dianastratan@gmail.com>
2022-10-03 19:52:12 +0200

    ThermometerViewController methods implemented

--- src/view/thermometer/ThermometerViewController.java
+++ src/view/thermometer/ThermometerViewController.java
@@ -1,4 +1,46 @@
 package view.thermometer;
 
-public class ThermometerViewController {
+import core.ModelFactory;
+import core.ViewHandler;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+import model.TemperatureListModelManager;
+import view.heater.HeaterViewModel;
+
+public class ThermometerViewController
+{
+  public Label labelThermometer1Value;
+  public Label labelThermometer2Value;
+  public Button btnBack;
+
+  private ViewHandler viewHandler;
+  private ThermometerViewModel modelManager;
+  private Region root;
+
+  public void init(ViewHandler viewHandler, ThermometerViewModel modelManager,
+      Region root)
+  {
+    this.viewHandler = viewHandler;
+    this.modelManager = modelManager;
+    this.root = root;
+  }
+
+  public Region getRoot()
+  {
+    return root;
+  }
+
+  public void handleForBookings(ActionEvent event)
+  {
+    if (event.getSource() == btnBack)
+    {
+      viewHandler.openView("HeaterView");
+    }
+    //else if (event.getSource() != btnBack) {}
+
+
+
+  }
 }
--- src/view/thermometer/ThermometerViewModel.java
+++ src/view/thermometer/ThermometerViewModel.java
@@ -1,4 +1,26 @@
 package view.thermometer;
 
+import javafx.beans.property.StringProperty;
+
 public class ThermometerViewModel {
+
+  private StringProperty thermometer1;
+  private StringProperty thermometer2;
+  private StringProperty alert;
+
+  public ThermometerViewModel() {
+
+  }
+
+
+  public StringProperty getThermometer1() {
+    return thermometer1;
+  }
+  public StringProperty getThermometer2() {
+   return thermometer2;
+  }
+
+  public StringProperty getAlert() {
+    return alert;
+  }
 }
